463dd18df8ddcfab9961443d6eb0e49743d6c74f
EJMQ6586 <joaquinmellado.kabeli@itau.cl>
2023-07-26 21:59:22 -0400

    init: lua configs

---
+++ init.lua
@@ -0,0 +1,26 @@
+require('jamq')
+vim.cmd("set noswapfile")
+
+
+
+--- Reload the entire configuration
+function reload_config()
+    for name,_ in pairs(package.loaded) do
+        if name:match('^jamq') then
+            package.loaded[name] = nil
+        end
+    end
+
+    require('jamq')
+
+    -- Reload after/ directory
+    local glob = vim.fn.stdpath('config') .. '/after/**/*.lua'
+    local after_lua_filepaths = vim.fn.glob(glob, true, true)
+
+    for _, filepath in ipairs(after_lua_filepaths) do
+        dofile(filepath)
+    end
+
+    vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+end
+vim.keymap.set('n', '<leader><leader>r', reload_config)
